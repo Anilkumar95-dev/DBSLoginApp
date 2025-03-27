160e787c7eca6ce2fbc62b7e586db6b1f5d9f157
Anilkumar95-dev <anilkumarpalabindela@gmail.com>
2025-03-27 21:01:41 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBSLoginApp.DAL;
+namespace DBSLoginApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserDAL userDAL = new UserDAL();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (userDAL.ValidateUser(username, password))
+            {
+                Session["Username"] = username;
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password.";
+                return View();
+            }
+        }
+
+        // GET: Dashboard
+        public ActionResult Dashboard()
+        {
+            if (Session["Username"] != null)
+            {
+                ViewBag.Username = Session["Username"].ToString();
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+
+        // Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Models/UserDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using DBSLoginApp.Models;
+
+namespace DBSLoginApp.DAL
+{
+    public class UserDAL
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DBSConnection"].ConnectionString;
+
+        public bool ValidateUser(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(1) FROM Users WHERE Username=@Username AND Password=@Password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+}
